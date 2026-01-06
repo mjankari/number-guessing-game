6860d620805f7de705d01fb54bd3d3e0bfabfc08
Mahdi Jankari <contact@mjankari.dev>
2026-01-06 15:15:20 +0300

    display game rules and welcome message

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import sys
 
 
 def main():
-    print("hello world")
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print("You have 5 chances to guess the correct number.")
+
 
 if __name__ == "__main__":
     sys.exit(main())
