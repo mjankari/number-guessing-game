68cd7fc1b1f3f35d52273a185c60a930777d7bae
Mahdi Jankari <contact@mjankari.dev>
2026-01-06 15:20:51 +0300

    generate random number to guess

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import sys
 
 
@@ -5,6 +6,9 @@ def main():
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
     print("You have 5 chances to guess the correct number.")
+
+    number = random.randrange(1, 100, 1)
+    print(number)
 
 
 if __name__ == "__main__":
